5e8a75110800e49e52bc0a166abec91034f432a4
farhan 27007 <muhammadfarhan27007@gmail.com>
2021-01-20 21:51:32 +0700

    penambahan section layanan

---
+++ tes.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" type="images/png" href="images/rsa_icon.png"> 
+  <title>Cek Hasil Laboratorium</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+  
+
+</head>
+<style type="text/css">
+.contoh1 { font-size:16px; }
+.contoh2 { font-size:16px; line-height: 1px;}
+.contoh3 { font-size:16px; line-height: 1.5em;}
+.contoh4 { font-size:16px; line-height: 2;}
+.title-tabel { font-weight: bold; }
+
+</style>
+<body>
+  <!-- Masthead -->
+  
+  <!-- Call to Action -->
+  <section class="call-to-action text-black">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto text-center">
+          <h2 class=" pt-5 mb-5 text-center"><b><ins>HASIL PEMERIKSAAN</b></ins></h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="row">
+        <div class="col-6" line-height:1px>
+          <table class="table text-lg-left" >
+            <tbody>
+
+              <tr>
+                <td class="w-100 border-top-0"><span class="title-tabel">NO SLIP :</span>
+                  <span>0000</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td class="w-100 border-top-0"><span class="title-tabel">NO REG :</span>
+                  <span>93014</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td class="w-100 border-top-0"><span class="title-tabel">NO PASIEN :</span>
+                  <span>906914</span>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-100 border-top-0"><span class="title-tabel">NAMA PASIEN :</span>
+                  <span>SITI FATIMAH</span>
+                </td>
+              </tr>
+              <tr>
+                <td class="w-100 border-top-0"><span class="title-tabel">TANGGAL LAHIR :</span>
+                  <span>1988-02-05</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="col-6" >
+          <table class="table text-lg-left">
+            <tbody>
+
+              <tr>
+                <td class="w-100 border-top-0"><span class="title-tabel">JENIS KELAMIN :</span>
+                  <span>P</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td class="w-100 border-top-0"><span class="title-tabel">ALAMAT :</span>
+                  <span>NEGARA NABUNG</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td class="w-100 border-top-0"><span class="title-tabel">WAKTU :</span>
+                  <span>2020-12-18</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td class="w-100 border-top-0"><span class="title-tabel">BANGSAL :</span>
+                  <span>R.JALAN</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td class="w-100 border-top-0"><span class="title-tabel">DOKTER :</span>
+                  <span>RONALD LISAL</span>
+                </td>
+              </tr>
+
+            </table>
+          </div>
+        </div>
+
+      </div>
+
+      <br><br>
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <!-- <h2 class="mb-4 text-center">Hasil Lab Anda</h2> -->
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <table class="table text-lg-left">
+          <thead>
+            <p><b><ins>COVID-19</ins></b></p>
+            <tr>
+              <th> PEMERIKSAAN</th>
+              <th> HASIL</th>
+              <th> NILAI RUJUKAN</th>
+              <th> SATUAN</th>
+              <th> KETERANGAN</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Rapid Tes Antibodi</td>
+              <td>Non Reaktif</td>
+              <td>Negatif</td>
+              <td>-</td>
+              <td>-</td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+      <div class="container mt-5 pt-5">
+       <div class="row">
+         <div class="col-8"></div>
+         <div class="col-4 text-center">
+           <br>
+           <br>
+           <b>PETUGAS PEMERIKSA</b>
+           <p class="mt-5 pt-5 pb-4">DAVID JUNJUNGAN MANULLANG</p>
+         </div>
+       </div>
+     </div>
+   </section>
+   <div class="container mt-5 pt-5">
+    <p><b><ins>CATATAN</ins></b></p>
+    <p><b>Hasil Negatif :</b></p>
+
+    <ul>
+      <li>Lakukan isolasi mandiri bila memiliki gejala infeksi saluran pernafasan atas ringan</li>
+      <li>Bila gejala makin berat disarankan untuk datang ke fasilitas kesehatan dan dilakukan tes kembali</li>
+      <li>Patuhi protokol kesehatan</li>
+    </ul>
+
+    <br>
+
+    <p><b>Hasil Positif :</b></p>
+    <ul>
+     <li>Lakukan isolasi mandiri secara ketat dan patuhi protokol kesehatan</li>
+     <li>Disarankan untuk mengikuti Swab Tes PCR</li>
+     <li>Bila gejala memburuk, segera datang ke Rumah Sakit</li>
+   </ul>
+
+   <br>
+   
+   <p class="contoh2">_rs.Advent bdl_</p>
+ 
+<!-- <script>
+		window.print();
+	</script> -->
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
